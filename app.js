e98229c9f58b20d164e640ef88555d730e259e3b
Lukas Vesely <lukas.v@remoteorigin.co>
2017-06-29 17:34:30 +0200

    get functional code from feature/issue-clone branch

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const moment = require('moment')
 const read = require('read')
 const chalk = require('chalk')
 const argv = require('yargs')
-  .usage('Usage: git-issues-downloader [options] URL \nType git-issues-downloader --help to see a list of all options.')
+  .usage('Usage: git-issues-downloader [options] URL \nType git-issues-downloader --help to see a list of all options. ')
   .help('h')
 
   .version(function () {
@@ -23,6 +23,7 @@ const argv = require('yargs')
   .describe('username', 'Your GitHub username')
   .describe('password', 'Your GitHub password')
   .describe('filename', 'Name of the output file')
+  .describe('clone', 'Url of repository where you want to clone your issues')
   .default('filename', 'all_issues.csv')
   .argv
 
@@ -30,10 +31,42 @@ const outputFileName = argv.filename
 
 // callback function for getting input from prompt
 
-getAuth = function (auth, silent, callback) {
+const getUserInput = function (auth, silent, callback) {
   read({prompt: `${auth}: `, silent: silent}, function (er, password) {
     callback(password)
   })
+}
+
+// function for getting username and password
+
+const getAuthorization = function (callback) {
+  let username = argv.username
+  let password = argv.password
+
+  if (username && password) {
+    callback(username, password)
+  } else if (username && !password) {
+    getUserInput('password', true, (passwordConsoleInput) => {
+      password = passwordConsoleInput
+
+      callback(username, password)
+    })
+  } else if (!username && password) {
+    getUserInput('username', true, (usernameConsoleInput) => {
+      username = usernameConsoleInput
+
+      callback(username, password)
+    })
+  } else if (!username && !password) {
+    getUserInput('username', false, (usernameConsoleInput) => {
+      username = usernameConsoleInput
+      getUserInput('password', true, (passwordConsoleInput) => {
+        password = passwordConsoleInput
+
+        callback(username, password)
+      })
+    })
+  }
 }
 
 // callback function for getting requested options
@@ -41,80 +74,62 @@ getAuth = function (auth, silent, callback) {
 const getRequestedOptions = exports.getRequestedOptions = function (username, password, url, callback) {
   const requestOptions = {
     headers: {
-      'User-Agent': 'request'
+      'User-Agent': 'lukasvesely98',
+      'Content-Type': 'application/json'
     },
     url: '',
     auth: {
       'user': '',
       'pass': ''
-    }
+    },
+    body: JSON.stringify({title: 'Issue'})
   }
 
   requestOptions.url = url
-
-  if (username && password) {
-    requestOptions.auth.user = username
-    requestOptions.auth.pass = password
-    callback(requestOptions)
-  } else {
-    if (password) {
-      requestOptions.auth.pass = password
-      getAuth('username', false, (usernameConsoleInput) => {
-        requestOptions.auth.user = usernameConsoleInput
-
-        callback(requestOptions)
-      })
-    } else {
-      if (username) {
-        requestOptions.auth.user = username
-        getAuth('password', true, (passwordConsoleInput) => {
-          requestOptions.auth.pass = passwordConsoleInput
-
-          callback(requestOptions)
-        })
-      } else {
-        getAuth('username', false, (usernameConsoleInput) => {
-          requestOptions.auth.user = usernameConsoleInput
-          getAuth('password', true, (passwordConsoleInput) => {
-            requestOptions.auth.pass = passwordConsoleInput
-
-            callback(requestOptions)
-          })
-        })
-      }
-    }
-  }
+  callback(requestOptions)
 }
+
+// take url a create api url from it
+
+const makeAPIUrl = function (url) {
+  const repoUserName = url.slice(19, url.indexOf('/', 19))
+  const repoUrl = (url.slice(20 + repoUserName.length, url.lastIndexOf('/'))) ? url.slice(20 + repoUserName.length, url.lastIndexOf('/')) : url.slice(20 + repoUserName.length)
+
+  return `https://api.github.com/repos/${repoUserName}/${repoUrl}`
+}
+
 
 // main function for running program
 
 const main = exports.main = function (data, requestedOptions) {
-  logExceptOnTest('Requesting API...')
-  requestBody(requestedOptions, (error, response, body) => {
-    linkObject = responseToObject(response.headers)
+  console.log('main')
+  getAuthorization((username, password) => {
+    requestedOptions.auth.username = username
+    requestedOptions.auth.password = password
+    logExceptOnTest('Requesting API...')
 
-    // take body, parse it and add it to data
+      // take body, parse it and add it to data
 
-    data = _.concat(data, body)
+      if (linkObject.nextPage) {
+        logExceptOnTest(chalk.green(`Successfully requested ${linkObject.nextPage.number - 1}. page of ${linkObject.lastPage.number}`))
+        requestedOptions.url = linkObject.nextPage.url
+        main(data, requestedOptions)
+      } else {
+        logExceptOnTest(chalk.green('Successfully requested last page'))
 
-    if (linkObject.nextPage) {
-      logExceptOnTest(chalk.green(`Successfully requested ${linkObject.nextPage.number - 1}. page of ${linkObject.lastPage.number}`))
-      requestedOptions.url = linkObject.nextPage.url
-      main(data, requestedOptions)
-    } else {
-      logExceptOnTest(chalk.green('Successfully requested last page'))
+        // else convert data and write them to csv file
+        logExceptOnTest('\nConverting issues...')
+        const csvData = convertJSonToCsv(issues)
 
-      logExceptOnTest('\nConverting issues...')
-      const csvData = convertJSonToCsv(data)
-      logExceptOnTest(chalk.green(`\nSuccessfully converted ${data.length} issues!`))
+        logExceptOnTest(chalk.green(`\nSuccessfully converted ${issues.length} issues!`))
 
-      logExceptOnTest('\nWriting data to csv file')
-      fs.writeFile(outputFileName, csvData, (err) => {
-        if (err) throw err
+        logExceptOnTest('\nWriting data to csv file')
+        fs.writeFile(outputFileName, csvData, (err) => {
+          if (err) throw err
 
-        logExceptOnTest(chalk.yellow(`\nIssues was downloaded, converted and saved to ${outputFileName}`))
-      })
-    }
+          logExceptOnTest(chalk.yellow(`\nIssues was downloaded, converted and saved to ${outputFileName}`))
+        })
+      }
   })
 }
 
@@ -145,7 +160,30 @@ const responseToObject = exports.responseToObject = function (response) {
   return false
 }
 
-// use url and request api
+      requestComments(issue.comments_url, requestedOptions, (error, response, body) => {
+
+        JSONBody = JSON.parse(body)
+
+        newIssues[index].comments_url=JSONBody
+
+        if (JSONBody.length) {
+          const comments = JSONBody.map(object => {
+            return {
+              body: object.body
+            }
+
+          })
+          
+// request for getting all comments from issue
+
+const requestComments = function (commentsUrl, requestedOptions, callback) {
+  requestedOptions.url = commentsUrl
+  request.get(requestedOptions, function (err, response, body) {
+    callback(err, response, body)
+  })
+}
+
+//request for getting body with issues and convert it to JSON
 
 const requestBody = exports.requestBody = function (requestedOptions, callback) {
   request.get(requestedOptions, function (err, response, body) {
@@ -162,7 +200,8 @@ const requestBody = exports.requestBody = function (requestedOptions, callback) 
           logExceptOnTest(chalk.red('\nYour username or password is invalid, please check it'))
           break
         default:
-          logExceptOnTest(chalk.red('\nRepository have 0 issues. Nothing to download'))
+          if (!argv.clone) logExceptOnTest(chalk.red('\nRepository have 0 issues. Nothing to download'))
+          else (callback(err, response, JSObject))
       }
     } else {
       callback(err, response, JSObject)
@@ -185,11 +224,9 @@ const convertJSonToCsv = exports.convertJSonToCsv = function (jsData) {
 
 const execute = exports.execute = function (argvRepository) {
   if (argvRepository) {
-    const issuesPerPage = 100
-    const repoUserName = argvRepository.slice(19, argvRepository.indexOf('/', 19))
-    const repoUrl = (argvRepository.slice(20 + repoUserName.length, argvRepository.lastIndexOf('/'))) ? argvRepository.slice(20 + repoUserName.length, argvRepository.lastIndexOf('/')) : argvRepository.slice(20 + repoUserName.length)
+    const issuesPerPage = 10
 
-    const startUrl = `https://api.github.com/repos/${repoUserName}/${repoUrl}/issues?per_page=${issuesPerPage}&state=all&page=1`
+    const startUrl = `${makeAPIUrl(argvRepository)}/issues?per_page=${issuesPerPage}&state=all&page=1`
 
     getRequestedOptions(argv.username, argv.password, startUrl, (requestedOptions) => {
       main([], requestedOptions)
